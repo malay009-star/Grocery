83248122cb7196e2659106b97e5e4092ca21c108
malay009-star <117511789+malay009-star@users.noreply.github.com>
2023-05-29 17:30:59 +0500

    we change grocery list

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,32 +1,78 @@
 var error_msg = document.getElementById('error_msg');
-
-var input = document.getElementById('input');
-
-var btn = document.getElementById('btn');
-console.log(btn);
-
-var itemBox = document.getElementById('item-box');
-
-var item = document.getElementById('item');
-
+var groceryList = document.getElementById('groceryList');
+var all_item = document.getElementById('all_item');
 var Dell = document.getElementById('Dell');
+let cleare_item = document.getElementById('cleare_item');
+console.log(cleare_item);
+counter = 0;
 
-function showItem() {
-    if (input.value === "") {
-        error_msg.textContent = 'please enter item';
-        error_msg.style.display = 'block';
+var userInput = document.getElementById('userInput');
+// console.log(userInput);
+
+// btn info
+document.getElementById('btn').addEventListener('click', () => {
+    if (userInput.value === "") {
+        error_msg.innerHTML = "please enter an items";
+        error_msg.classList.add('bg-red-100');
     } else {
-        error_msg.style.display = 'block';
-        error_msg.innerHTML = 'message has been recievd';
-        error_msg.style.background = '#49BE25';
-        error_msg.style.color = 'white';
-        item.innerHTML = input.value;
-        itemBox.style.display = 'block';
-        itemBox.style.display = 'flex';
+        error_msg.innerHTML = "items successfully added";
+        error_msg.classList.add('bg-green-100');
+
+        // cleare item
+        cleare_item.innerHTML = 'cleare items';
+        cleare_item.classList.add('mt-10');
+        groceryList.classList.add('mt-10');
+
+        // create element and add classes
+        let div = document.createElement('div');
+        let del = div.classList.add('item_div' + counter);
+
+        let x = document.createElement('p');
+        div.classList.add('bg-blue-100');
+        div.classList.add('mt-6');
+        div.classList.add('px-4');
+        div.classList.add('py-1');
+        div.classList.add('rounded');
+        div.classList.add('flex');
+        div.classList.add('justify-between');
+
+        x.classList.add('item' + counter);
+        value = userInput.value;
+        x.innerHTML = value;
+        div.appendChild(x);
+        groceryList.appendChild(div);
+
+        // for image
+        var img = new Image();
+        // Set the source of the image
+        img.src = "Assets/img/trash.png"
+        img.classList.add('item' + counter);
+        div.appendChild(img);
+        groceryList.appendChild(div);
+
+        // remove item oncling image
+        img.addEventListener('click', () => {
+                if (img.className === x.className) {
+                    // div.style.display = 'none';
+                    div.remove(del);
+                }
+            })
+            // counter
+        console.log(div);
+        counter++;
+
+        // clear item info
+        cleare_item.addEventListener('click', () => {
+            groceryList.innerHTML = '';
+            if (groceryList.innerHTML === '') {
+                error_msg.innerHTML = 'item has been deleted';
+                error_msg.classList.add('bg-red-200');
+                cleare_item.innerHTML = '';
+                cleare_item.classList.add('mt-0')
+            } else {
+                error_msg.innerHTML = 'item has been added';
+                error_msg.classList.add('bg-green-200');
+            }
+        })
     }
-}
-Dell.addEventListener('click', () => {
-    itemBox.style.display = 'none';
-    error_msg.style.display = 'none';
-    input.value = "";
 })
